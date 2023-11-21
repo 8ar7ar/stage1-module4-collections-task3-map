d9436e86570a6f62f835df9c22146f5af2cb4fb2
8ar7ar <5ar7ar@mail.ru>
2023-11-21 18:16:30 +0500

    stage1-module4-collections-task3-map " 1st attempt--locally tested "

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new LinkedHashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet())
+
+            if (swappedMap.get(entry.getValue()) == null ||
+                    swappedMap.get(entry.getValue()) > entry.getKey()){
+                swappedMap.put(entry.getValue(), entry.getKey());
+            }
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        StringTokenizer tokens = new StringTokenizer(sentence,
+                                                    " ,.",
+                                                    false);
+        List<String> words = new ArrayList<>();
+        while(tokens.hasMoreTokens())
+            words.add(tokens.nextToken().toLowerCase());
+
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+
+        for (int i = 0; i < words.size(); i++) {
+            int count = 1;
+            String word = words.get(i);
+            for (int j = i+1; j < words.size(); j++){
+                if (word.equals(words.get(j))) {
+                    words.remove(j);
+                    count++;
+                }
+            }
+            wordRepetitionMap.put(word, count);
+        }
+        return  wordRepetitionMap;
     }
 }
